5e46598e43663162f7f2c2f1f406a3feb6a213d2
michaeljord92 <michaeljord92@hotmail.com>
2021-04-07 20:36:01 -0300

    Funcionando para apenas um usuario

--- administrador/Admin.py
+++ administrador/Admin.py
@@ -5,26 +5,60 @@ class Admin(object):
     usuarios = []
     arquivos = {}
 
-    def registraUsuario(self, nome):
-        self.usuarios.insert(nome)
-        status = "Usuario registrado: "+ nome
+    
+    def getUsuarios(self):
+        '''
+        Retorna lista de usuario
+        '''
+        return self.usuarios
+
+    def indicaNomeUsuario(self):
+        '''
+        Indica um novo nome de usuario
+        '''
+        nomeUsuario = "usuarioA"
+        print("Nome de usuario indicado: "+nomeUsuario)
+        return nomeUsuario
+
+    def registrarUsuario(self,nomeUsuario):
+        '''
+        Registra usuario novo à lista de usuarios
+        '''
+        # self.usuarios.insert(nomeUsuario)
+        self.usuarios.append(nomeUsuario)
+
+        status = "Usuario registrado: "+ nomeUsuario
+        print(status)
+        
         return status
     
-    def registraArquivo(self, nome):
-        status = "Arquivo registrado: "+ nome    
+    def registrarArquivo(self, nomeArquivo, nomeUsuario):
+        '''
+        Registra um novo arquivo da rede e seu correspondente usuario
+        onde está hospedado
+        '''
+        self.arquivos
+        status = "Arquivo registrado: "+ nomeArquivo    
+        print(status)
         return status
 
-    def listaArquivos(self):
+    def listarArquivos(self):
+        '''
+        Retorna a lista de arquivos e seus correspondente usuarios hospedeiros
+        '''
         return self.arquivos
     
-    def realocaArquivo(self, usuario_origem, usuario_destino):
+    def realocarArquivo(self, usuarioOrigem, usuarioDestino):
+        '''
+        Redistribui os aquivos entre os usuários
+        '''
         status = "realocado"
         return status    
 
 
 daemon = Pyro5.server.Daemon()
 servidor_nome = Pyro5.api.locate_ns()
-servidor_nome.register("livros",daemon.register(Admin))
+servidor_nome.register("admin",daemon.register(Admin))
 
-print("Admin em loop.")
+print("Administrador em loop.")
 daemon.requestLoop()
--- usuario/Usuario.py
+++ usuario/Usuario.py
@@ -1,34 +1,108 @@
 import Pyro5.api
 
+
+@Pyro5.api.expose
 class Usuario(object):
     nome = ""
     arquivos = []
 
-    def registraUsuario(self):
+    def __init__(self, nomeUsuario):
+        self.nome = nomeUsuario
+        print("Iniciado novo usuario: "+self.nome)
+        
+        # Registra o usuario no administrador
         admin = Pyro5.api.Proxy("PYRONAME:admin")
-        status = admin.registraUsuario()
+        status = admin.registrarUsuario(self.nome)
+        print(status)
+
+    def getNome(self):
+        print("Get Nome: "+self.nome)
+        return self.nome
+
+    def registrarUsuario(self):
+        '''
+        Registra este usuario no administrador
+        '''
+        admin = Pyro5.api.Proxy("PYRONAME:admin")
+        nomeUsuario = admin.registrarUsuario()
+        self.nome = nomeUsuario
+
+        status = "Usuario registrado"
         return status
 
-    def listaArquivos(self):
-        admin = Pyro5.api.Proxy("PYRONAME:admin")  # Busca referencia no servidor de nomes
-        lista_de_livros = admin.lista()
+    def listarArquivos(self):
+        '''
+        Lista arquivos catalogados pelo administrador
+        '''
+        admin = Pyro5.api.Proxy(
+            "PYRONAME:admin")  # Busca referencia no servidor de nomes
+        lista_de_livros = admin.listarArquivos()
 
         resposta = "Lista de livros:\n"
         resposta = resposta + lista_de_livros
-        
-        # print resposta
-        return resposta
+        print(resposta)
 
-    def carregaArquivo(self,nome):
-        admin = Pyro5.api.Proxy("PYRONAME:admin")
-        conteudo = admin.carregar(nome)
+        return lista_de_livros
+
+    def carregarArquivo(self, nomeArquivo, nomeUsuario):
+        '''
+        Carrega arquivo para leitura
+        '''
+        usuario = Pyro5.api.Proxy("PYRONAME:"+nomeUsuario)
+        conteudo = usuario.enviarArquivo(nomeArquivo)
 
         # print conteudo
         return conteudo
 
-    def gravaArquivo(self, nome, conteudo):
-        admin = Pyro5.api.Proxy("PYRONAME:admin")
-        status = admin.enviar(nome,conteudo)
-        
-        # print status
+    def enviarArquivo(self, nomeArquivo):
+        '''
+        Retorna conteudo de arquivo local
+        '''
+        arquivo = open(file="./arquivos/"+nomeArquivo, mode='r')
+        conteudo = arquivo.readlines()
+        arquivo.close()
+
+        return conteudo
+
+    def gravarArquivo(self, nomeArquivo, conteudo):
+        '''
+        Grava arquivo localmente
+        '''
+        arquivo = open(file="./arquivos/"+nomeArquivo, mode='w')
+        arquivo.writelines(conteudo)
+        arquivo.close()
+
+        status = "Arquivo gravado"
+
+        print(status)
         return status
+
+    def gravarArquivoNovo(self, nomeArquivo, conteudo):
+        '''
+        Grava um novo arquivo localmente e informa o administrador
+        '''
+        # gravar arquivo localmente
+        self.gravarArquivo(nomeArquivo, conteudo)
+
+        # registrar arquivo no administrador
+        admin = Pyro5.api.Proxy("PYRONAME:admin")
+        status = admin.registrarArquivo(nomeArquivo, self.nome)
+
+        status = "Arquivo novo gravado"
+        print(status)
+        return status
+
+
+# Requesita um nome de usuario ao servidor
+admin = Pyro5.api.Proxy("PYRONAME:admin")
+nomeUsuario = admin.indicaNomeUsuario()
+print("Nome indicado pelo admin: " + nomeUsuario)
+
+# Registra usuario no servidor de nome e inicia-o
+daemon = Pyro5.server.Daemon()
+servidor_nome = Pyro5.api.locate_ns()
+servidor_nome.register(nomeUsuario, daemon.register(
+    Usuario(nomeUsuario)))
+
+print("Usuario "+nomeUsuario+" em loop.")
+daemon.requestLoop()
