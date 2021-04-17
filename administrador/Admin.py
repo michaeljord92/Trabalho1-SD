b3af1144f71ab9ae052dbbcf8aa8da129dab75d8
michaeljord92 <michaeljord92@hotmail.com>
2021-04-17 00:24:03 -0300

    Faltando o algoritmo de balanceamento no admin

--- administrador/Admin.py
+++ administrador/Admin.py
@@ -1,64 +1,142 @@
 import Pyro5.api
+import os
+import datetime
+
 
 @Pyro5.api.expose
 class Admin(object):
+    nome = "admin"
     usuarios = []
     arquivos = {}
+    limiteUsuarios = 50
 
-    
-    def getUsuarios(self):
+    def gravarLog(self, mensagem="") -> None:
+        '''
+        Grava mensagem no arquivo de log.
+        '''
+        # Criar diretorio para gravação do arquivo
+        diretorio = os.getcwd() + "/logs/"
+        try:
+            os.makedirs(diretorio, exist_ok=True)
+        except OSError:
+            status = "Erro na gravação de arquivo de log\n" + \
+                "Erro na criação de diretorio\n" + "Erro: "+str(OSError)
+            print(status)
+            # self.gravarLog(status)
+
+        # abrir arquivo para log
+        nomeArquivo = "log_"+self.nome+".log"
+        try:
+            arquivo = open(file=diretorio+nomeArquivo, mode='at')
+        except OSError:
+            status = "Erro ao abrir arquivo para log\n"+"Erro: "+str(OSError)
+            print(status)
+            # self.gravarLog(status)
+        else:
+            data = datetime.datetime.now()
+            _log = str(data)+" "+str(mensagem)
+            arquivo.write(_log+"\n")
+            arquivo.close()
+            # print("log: "+_log)
+
+    def getUsuarios(self) -> list:
         '''
         Retorna lista de usuario
         '''
         return self.usuarios
 
-    def indicaNomeUsuario(self):
+    def indicaNomeUsuario(self) -> str:
         '''
         Indica um novo nome de usuario
         '''
-        nomeUsuario = "usuarioA"
-        print("Nome de usuario indicado: "+nomeUsuario)
+        nomeUsuario = "usuario"+str(len(self.usuarios))
+        # print("Nome de usuario indicado: "+nomeUsuario)
         return nomeUsuario
 
-    def registrarUsuario(self,nomeUsuario):
+    def registrarUsuario(self, nomeUsuario="") -> dict:
         '''
         Registra usuario novo à lista de usuarios
-        '''
-        # self.usuarios.insert(nomeUsuario)
-        self.usuarios.append(nomeUsuario)
 
-        status = "Usuario registrado: "+ nomeUsuario
-        print(status)
-        
+        Retorna um "dict" com "mensagem" do tipo "string" e com "registro" do tipo "bool". 
+        '''
+        # verificar limite de usuarios
+        if len(self.usuarios) >= self.limiteUsuarios:
+            status = {"mensagem": "Usuario não registrado. Limite de " +
+                      str(self.limiteUsuarios) + " usuarios excedidos", "registro": False}
+            return status
+
+        # registra o usuario
+        self.usuarios.append(nomeUsuario)
+        mensagem = "Usuario registrado: " + str(nomeUsuario)
+        status = {"mensagem": mensagem, "registro": True}
+        self.gravarLog(mensagem)
+
         return status
-    
-    def registrarArquivo(self, nomeArquivo, nomeUsuario):
+
+    def registrarArquivo(self, nomeArquivo, nomeUsuario) -> dict:
         '''
         Registra um novo arquivo da rede e seu correspondente usuario
         onde está hospedado
         '''
-        self.arquivos
-        status = "Arquivo registrado: "+ nomeArquivo    
-        print(status)
-        return status
+        self.arquivos[nomeArquivo] = nomeUsuario
+        mensagem = "Arquivo registrado " + nomeArquivo+" do usuario " + nomeUsuario
+        resposta = {"status": True, "mensagem": mensagem}
 
-    def listarArquivos(self):
+        self.gravarLog(mensagem)
+        # print(mensagem)
+        return resposta
+
+    def listarArquivos(self) -> dict:
         '''
-        Retorna a lista de arquivos e seus correspondente usuarios hospedeiros
+        Retorna a lista de arquivos e seus 
+        correspondente usuarios hospedeiros
         '''
         return self.arquivos
-    
-    def realocarArquivo(self, usuarioOrigem, usuarioDestino):
+
+    def realocarArquivo(self, nomeArquivo, usuarioOrigem, usuarioDestino) -> dict:
         '''
-        Redistribui os aquivos entre os usuários
+        Realoca arquivo entre os usuários
         '''
-        status = "realocado"
-        return status    
+        origem = Pyro5.api.Proxy("PYRONAME:"+usuarioOrigem)
+        destino = Pyro5.api.Proxy("PYRONAME:"+usuarioDestino)
+
+        # manda usuario destino gravar uma copia
+        resposta = destino.gravarArquivo(
+            nomeArquivo, origem.enviarArquivo(nomeArquivo))
+
+        if resposta["status"]:
+            # manda usuario origem excluir
+            resposta = origem.excluirArquivo(nomeArquivo)
+            if resposta["status"]:
+                # atualiza a lista de arquivos e seus correspodentes usuario
+                self.arquivos[nomeArquivo] = usuarioDestino
+                mensagem = "Arquivo realocado do usuario " + \
+                    str(usuarioOrigem)+" para o "+str(usuarioDestino)
+                self.gravarLog(mensagem)
+                resposta = {"status":True,"mensagem":mensagem}
+            else:
+                mensagem = "Arquivo não removido do usuario origem."
+                resposta = {"status":False,"mensagem":mensagem}
+        else:
+            mensagem = "Arquivo não gravado pelo usuario destino."
+            resposta = {"status":False,"mensagem":mensagem}
+
+        
+        return resposta
+
+    def balancearArquivos(self):
+        '''
+        Identifica desbalanceamento de arquivos entre os usuarios 
+        e realiza balanceamento.
+        '''
+        # falta aqui implementar algum algoritimo de balanceamento
 
 
+# Registra administrador no servidor de nomes
 daemon = Pyro5.server.Daemon()
+
 servidor_nome = Pyro5.api.locate_ns()
-servidor_nome.register("admin",daemon.register(Admin))
+servidor_nome.register("admin", daemon.register(Admin))
 
 print("Administrador em loop.")
 daemon.requestLoop()
--- usuario/Usuario.py
+++ usuario/Usuario.py
@@ -1,108 +1,251 @@
 import Pyro5.api
+import os
+import datetime
 
 
 @Pyro5.api.expose
 class Usuario(object):
-    nome = ""
-    arquivos = []
+    nome = ""   # Nome de identificação de usuario no sistema
+    arquivos = []   # Lista de arquivos locais
+    registrado = False  # Registrado no administrador
 
-    def __init__(self, nomeUsuario):
-        self.nome = nomeUsuario
-        print("Iniciado novo usuario: "+self.nome)
-        
+    def __init__(self, nomeUsuario=""):
+
+        if nomeUsuario == "":
+            # Requesita um nome de identificação de usuario ao servidor
+            admin = Pyro5.api.Proxy("PYRONAME:admin")
+            self.nome = admin.indicaNomeUsuario()
+            status = "Nome indicado pelo admin: " + self.nome
+            # self.gravarLog(status)
+        else:
+            # Atribui nome de identifição de usuario
+            self.nome = nomeUsuario
+
+        status = "Iniciado novo usuario: "+self.nome
+        # self.gravarLog(status)
+
         # Registra o usuario no administrador
         admin = Pyro5.api.Proxy("PYRONAME:admin")
-        status = admin.registrarUsuario(self.nome)
-        print(status)
+        resposta = admin.registrarUsuario(self.nome)
+        status = "Registro no administrador: \n\tResposta do administrador: " + \
+            str(resposta["mensagem"])
+        # self.gravarLog(status)
+        self.registrado = resposta["registro"]
 
-    def getNome(self):
-        print("Get Nome: "+self.nome)
-        return self.nome
-
-    def registrarUsuario(self):
+    def gravarLog(self, mensagem="") -> None:
         '''
-        Registra este usuario no administrador
+        Grava mensagem no arquivo de log.
         '''
-        admin = Pyro5.api.Proxy("PYRONAME:admin")
-        nomeUsuario = admin.registrarUsuario()
-        self.nome = nomeUsuario
+        # Criar diretorio para gravação do arquivo
+        diretorio = os.getcwd() + "/logs/"
+        try:
+            os.makedirs(diretorio, exist_ok=True)
+        except OSError:
+            status = "Erro na gravação de arquivo de log\n" + \
+                "Erro na criação de diretorio\n" + "Erro: "+str(OSError)
+            print(status)
+            # self.gravarLog(status)
 
-        status = "Usuario registrado"
-        return status
+        # abrir arquivo para log
+        nomeArquivo = "log_"+self.nome+".log"
+        try:
+            arquivo = open(file=diretorio+nomeArquivo, mode='at')
+        except OSError:
+            status = "Erro ao abrir arquivo para log\n"+"Erro: "+str(OSError)
+            print(status)
+            # self.gravarLog(status)
+        else:
+            data = datetime.datetime.now()
+            _log = str(data)+" "+str(mensagem)
+            arquivo.write(_log+"\n")
+            arquivo.close()
+            # print("log: "+_log)
 
-    def listarArquivos(self):
+    # def getNome(self) -> str:
+    #     '''
+    #     Retorna nome de usuario
+    #     '''
+    #     status = "Get nome: "+self.nome
+    #     self.gravarLog(status)
+    #     return self.nome
+
+    # def getArquivos(self) -> list:
+    #     '''
+    #     Retorna lista de aquivos locais
+    #     '''
+    #     status = "Get arquivos: \n"+str(self.arquivos)
+    #     self.gravarLog(status)
+    #     return self.arquivos
+
+    def listarArquivos(self) -> str:
         '''
         Lista arquivos catalogados pelo administrador
         '''
-        admin = Pyro5.api.Proxy(
-            "PYRONAME:admin")  # Busca referencia no servidor de nomes
+        admin = Pyro5.api.Proxy("PYRONAME:admin")
+        listaArquivos = admin.listarArquivos()
+
+        print("Lista de livros: \n" + str(listaArquivos.keys()))
+        
+        mensagem = "Lista de livros lidas do administrador."
+        # self.gravarLog(mensagem)
+
+        return mensagem
+
+    def carregarArquivo(self, nomeArquivo) -> dict:
+        '''
+        Carrega conteudo de arquivo e exibe-o.
+        '''
+        # Busca lista de livros e seus correspodentes
+        admin = Pyro5.api.Proxy("PYRONAME:admin")
         lista_de_livros = admin.listarArquivos()
 
-        resposta = "Lista de livros:\n"
-        resposta = resposta + lista_de_livros
-        print(resposta)
-
-        return lista_de_livros
-
-    def carregarArquivo(self, nomeArquivo, nomeUsuario):
-        '''
-        Carrega arquivo para leitura
-        '''
+        # Requisita ao correspondente o conteudo do arquivo
+        nomeUsuario = lista_de_livros[nomeArquivo]
         usuario = Pyro5.api.Proxy("PYRONAME:"+nomeUsuario)
-        conteudo = usuario.enviarArquivo(nomeArquivo)
+        resposta = usuario.enviarArquivo(nomeArquivo)
+        if resposta["status"]:
+            print(resposta["conteudo"])
+            mensagem = "Arquivo "+str(nomeArquivo) + \
+                " lido do usuario "+str(nomeUsuario)
+            self.gravarLog(mensagem)
+            resposta = {"status": True, "mensagem": mensagem}
+        else:
+            mensagem = "Arquivo "+str(nomeArquivo) + \
+                " não lido do usuario "+str(nomeUsuario)
+            # self.gravarLog(mensagem)
+            resposta = {"status": False, "mensagem": mensagem}
 
-        # print conteudo
-        return conteudo
+        return resposta
 
-    def enviarArquivo(self, nomeArquivo):
+    def enviarArquivo(self, nomeArquivo) -> dict:
         '''
-        Retorna conteudo de arquivo local
+        Retorna conteudo de arquivo local.
+
+        Retorna um "dict" com "status" de leitura e um "conteudo" 
+        com a leitura do arquivo local ou com a mensagem de erro.
         '''
-        arquivo = open(file="./arquivos/"+nomeArquivo, mode='r')
-        conteudo = arquivo.readlines()
-        arquivo.close()
+        diretorio = os.getcwd() + "/arquivos/"+self.nome+"/"
+        try:
+            arquivo = open(file=diretorio+nomeArquivo, mode='r')
+        except IOError:
+            mensagem = "Erro no envio de arquivo\n"+"Erro: "+str(IOError)
+            resposta = {"status": False, "conteudo": mensagem}
+            # self.gravarLog(mensagem)
+        else:
+            conteudo = arquivo.readlines()
+            arquivo.close()
+            resposta = {"status": True, "conteudo": conteudo}
+            status = "Arquivo "+str(nomeArquivo)+" lido e conteudo enviado."
+            self.gravarLog(status)
 
-        return conteudo
+        return resposta
 
-    def gravarArquivo(self, nomeArquivo, conteudo):
+    def gravarArquivo(self, nomeArquivo="", conteudo="") -> dict:
         '''
         Grava arquivo localmente
+
+        Retorna "dict" com "status" de gravação e com "mensagem".
         '''
-        arquivo = open(file="./arquivos/"+nomeArquivo, mode='w')
-        arquivo.writelines(conteudo)
-        arquivo.close()
+        # Criar diretorio para gravação do arquivo
+        diretorio = os.getcwd() + "/arquivos/"+self.nome+"/"
+        try:
+            os.makedirs(diretorio, exist_ok=True)
+        except OSError:
+            mensagem = "Erro na gravação de arquivo\n" + \
+                "Erro na criação de diretorio\n" + "Erro: "+str(OSError)
+            resposta = {"status": False, "mensagem": mensagem}
+            # self.gravarLog(mensagem)
 
-        status = "Arquivo gravado"
+        # Abrir arquivo e gravar conteudo
+        try:
+            arquivo = open(file=diretorio+nomeArquivo, mode='w')
+        except IOError:
+            mensagem = "Erro na gravação de arquivo\n" + "Erro: "+str(IOError)
+            # self.gravarLog(mensagem)
+            resposta = {"status": False, "mensagem": mensagem}
+        else:
+            arquivo.writelines(conteudo)
+            arquivo.close()
+            self.arquivos.append(nomeArquivo)
 
-        print(status)
-        return status
+            mensagem = "Arquivo "+nomeArquivo + " gravado"
+            # self.gravarLog(mensagem)
+            resposta = {"status": True, "mensagem": mensagem}
 
-    def gravarArquivoNovo(self, nomeArquivo, conteudo):
+        print(mensagem["mensagem"])
+        return resposta
+
+    def gravarArquivoNovo(self, nomeArquivo="", conteudo="") -> dict:
         '''
         Grava um novo arquivo localmente e informa o administrador
         '''
         # gravar arquivo localmente
-        self.gravarArquivo(nomeArquivo, conteudo)
+        resposta = self.gravarArquivo(nomeArquivo, conteudo)
+        if resposta["status"]:
+            # registrar arquivo no administrador
+            admin = Pyro5.api.Proxy("PYRONAME:admin")
+            resposta = admin.registrarArquivo(nomeArquivo, self.nome)
+            if resposta["status"]:
+                mensagem = "Arquivo novo " + \
+                    str(nomeArquivo)+" gravado localmente e registrado no administrador."
+                self.gravarLog(mensagem)
+                resposta = {"status": True, "mensagem": mensagem}
+            else:
+                mensagem = "Arquivo novo " + \
+                    str(nomeArquivo) + \
+                    " gravado localmente mas não registrado no administrador."
+                # self.gravarLog(mensagem)
+                resposta = {"status": False, "mensagem": mensagem}
+        else:
+            mensagem = "Arquivo novo " + \
+                str(nomeArquivo)+" não gravado localmente."
+            # self.gravarLog(mensagem)
+            resposta = {"status": False, "mensagem": mensagem}
 
-        # registrar arquivo no administrador
-        admin = Pyro5.api.Proxy("PYRONAME:admin")
-        status = admin.registrarArquivo(nomeArquivo, self.nome)
+        return resposta
 
-        status = "Arquivo novo gravado"
-        print(status)
-        return status
+    def excluirArquivo(self, nomeArquivo="") -> dict:
+        '''
+        Excluir um arquivo localmente
+        '''
+        # verifica existencia do arquivo
+        diretorio = os.getcwd() + "/arquivos/"+self.nome+"/"
+        if os.path.exists(diretorio+nomeArquivo):
+            try:
+                # exclui o arquivo local
+                os.remove(diretorio+nomeArquivo)
+            except OSError:
+                mensagem = "Erro na exclusão do arquivo " + \
+                    str(nomeArquivo) + "\n" + "Erro: "+str(OSError)
+                # self.gravarLog(mensagem)
+                resposta = {"status": False, "mensagem": mensagem}
+            else:
+                self.arquivos.remove(nomeArquivo)
+                mensagem = "Arquivo excluído: "+str(nomeArquivo)
+                # self.gravarLog(mensagem)
+                resposta = {"status": True, "mensagem": mensagem}
+        else:
+            mensagem = "Arquivo "+str(nomeArquivo)+" não encontrado."
+            # self.gravarLog(mensagem)
+            resposta = {"status": False, "mensagem": mensagem}
+
+        return resposta
 
 
-# Requesita um nome de usuario ao servidor
-admin = Pyro5.api.Proxy("PYRONAME:admin")
-nomeUsuario = admin.indicaNomeUsuario()
-print("Nome indicado pelo admin: " + nomeUsuario)
+# Instancia usuario
+usuario = Usuario()
 
-# Registra usuario no servidor de nome e inicia-o
-daemon = Pyro5.server.Daemon()
-servidor_nome = Pyro5.api.locate_ns()
-servidor_nome.register(nomeUsuario, daemon.register(
-    Usuario(nomeUsuario)))
+if usuario.registrado:
+    # Registra usuario no servidor de nomes
+    daemon = Pyro5.server.Daemon()
+    servidor_nome = Pyro5.api.locate_ns()
+    servidor_nome.register(usuario.nome, daemon.register(usuario))
+    status = "Usuario "+usuario.nome+" registrado no servidor de nomes."
+    # usuario.gravarLog(status)
 
-print("Usuario "+nomeUsuario+" em loop.")
-daemon.requestLoop()
+    status = "Usuario "+usuario.nome+" em loop."
+    # usuario.gravarLog(status)
+    daemon.requestLoop()
+else:
+    status = "Usuario não registrado no administrador."
+    # usuario.gravarLog(status)
